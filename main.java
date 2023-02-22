72e9dcc2de6091e044aa7ec4fba5085e128a9f7e
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-02-22 09:49:43 +0100

    Añadir lectura de datos

--- main.java
+++ main.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
 public class main{
     public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Este programa dibuja una piramide");
 	// Creacion de variables
-	int alt = 7;
+	int alt = sc.nextInt();
 	int e = alt -1;
 	int s = 0;
 	int cont = 1;
