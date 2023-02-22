3d9b96078ef56587f914e698f6e850fe6b6df9b7
PabloGM1204 <pgarmun203@g.educaand.es>
2023-02-22 09:26:02 +0100

    Creación de las variables

--- main.java
+++ main.java
@@ -1,5 +1,10 @@
-public class Ejercicio19{
+public class main{
     public static void main(String [] args) {
         System.out.println("Este programa dibuja una piramide");
+	// Creacion de variables
+	int alt = 7;
+	int e = alt -1;
+	int s = 0;
+	int cont = 1;
     }
 }
